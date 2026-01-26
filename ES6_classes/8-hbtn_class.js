65b1c46a407647f7b00b69d125a47cf8d49f4fe4
altintahiraj <altintahiraj357@gmail.com>
2026-01-26 01:12:20 +0100

    changes

---
+++ ES6_classes/8-hbtn_class.js
@@ -0,0 +1,17 @@
+export default class HolbertonClass {
+  constructor(size, location) {
+    this._size = size;
+    this._location = location;
+  }
+
+  // Kontrollon cast-in ne primitive
+  [Symbol.toPrimitive](hint) {
+    if (hint === 'number') {
+      return this._size;
+    }
+    if (hint === 'string') {
+      return this._location;
+    }
+    return this._location;
+  }
+}
